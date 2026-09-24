8ee6711a1819912703563ce13aaf99b5a5c746d7
Your Name <you@example.com>
2026-09-24 17:46:59 +0300

    first commit

---
+++ .py
@@ -0,0 +1,20 @@
+# varibales and data types
+name = "John"
+age = 20
+height = 178.65
+is_adult = False
+numbers = [3, 5, 7, 8, 4, 2]
+student = {"name": "John", "age": 30, "gender": "male"}
+
+print(name)
+print(type(name))
+print(age)
+print(type(age))
+print(height)
+print(type(height))
+print(is_adult)
+print(type(is_adult))
+print(numbers)
+print(type(numbers))
+print(student)
+print(type(student))
